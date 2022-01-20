3c5b511d45cd600a6ef4eb9d76f72a4c959911c5
Renato Regalado <renato.regalado@gmail.com>
2022-01-20 05:15:22 -0600

    finished pass_fail.go

--- chapter2/pass_fail.go
+++ chapter2/pass_fail.go
@@ -5,14 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	var status = ""
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(input)
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("a grade of", grade, "is", status)
 }
