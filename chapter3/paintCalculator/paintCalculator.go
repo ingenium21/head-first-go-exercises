41c24a57b82253ed1f55947f4d0709b4e30a0b0d
Renato Regalado <renato.regalado@gmail.com>
2022-02-01 16:03:08 -0600

    paint calculator exercise from chapter 3

---
+++ chapter3/paintCalculator/paintCalculator.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+//calculates the amount of liters needed, based on the supposition that 1 liter covers 10sqft
+// returns nothing
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("A height of %0.2f is invalid", height)
+	}
+
+	area := width * height
+	return area / 10.0, nil
+}
+
+func main() {
+	var amount, total float64
+	amount, err := paintNeeded(4.2, -3.0)
+	fmt.Printf("%0.2f liters needed\n", amount)
+	if err != nil {
+		log.Fatal(err)
+	}
+	total += amount
+	amount, err = paintNeeded(-5.2, 3.5)
+	fmt.Printf("%0.2f liters needed\n", amount)
+	fmt.Println(err)
+	total += amount
+	fmt.Printf("Total: %0.2f liters\n", total)
+}
