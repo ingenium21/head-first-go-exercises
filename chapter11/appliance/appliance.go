85b7b04911e7286ccface8adbf1414d206840a15
Renato Regalado <renato.regalado@gmail.com>
2022-03-21 14:59:47 -0500

    added some more stuff to chapter 11 to understand interfaces better

--- chapter11/appliance/appliance.go
+++ chapter11/appliance/appliance.go
@@ -1,11 +1,16 @@
 package main
 
 type Appliance interface {
+	Describe()
 	TurnOn()
 	TurnOff()
 }
 
 type Fan string
+
+func (f Fan) Describe() {
+	println("Fan is a", f)
+}
 
 func (f Fan) TurnOn() {
 	println("Fan is on")
@@ -16,6 +21,10 @@ func (f Fan) TurnOff() {
 }
 
 type CoffeePot string
+
+func (c CoffeePot) Describe() {
+	println("Coffee pot is a", c)
+}
 
 func (c CoffeePot) TurnOn() {
 	println("Coffee pot is on")
@@ -32,16 +41,24 @@ func (c CoffeePot) PourCoffee() {
 func main() {
 	//if you define a type as an interface, you can assign it to a variable of type interface
 	//but you can't use functions that are not defined in the interface
-	var device Appliance
-	device = Fan("Windco Breeze")
-	device.TurnOn()
-	device.TurnOff()
-	device = CoffeePot("LuxBrew")
-	device.TurnOn()
-	device.TurnOff()
+	// var device Appliance
+	// device = Fan("Windco Breeze")
+	// device.TurnOn()
+	// device.TurnOff()
+	// device = CoffeePot("LuxBrew")
+	// device.TurnOn()
+	// device.TurnOff()
+
+	for _, device := range []Appliance{Fan("Windco Breeze"), CoffeePot("LuxBrew")} {
+		device.Describe()
+		device.TurnOn()
+		device.TurnOff()
+	}
 
 	//if you assign it as a concrete type, you can use functions that are defined in the interface AND the concrete type
 	var dev1 CoffeePot
+	dev1 = CoffeePot("Black and Decker")
+	dev1.Describe()
 	dev1.TurnOn()
 	dev1.TurnOff()
 	dev1.PourCoffee()
